cae514b03a549607009c300076f558db9b4e61f4
Kuba <xzxpiksellzxz@gmail.com>
2016-11-25 20:37:11 +0100

    Alo

--- MainActivity.cs
+++ MainActivity.cs
@@ -11,7 +11,7 @@ namespace ekrikurv1
     [Activity(Label = "ekrikurv1", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -22,11 +22,15 @@ namespace ekrikurv1
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
-            
+            Button button = FindViewById<Button>(Resource.Id.button1);
+            var text = FindViewById<TextView>(Resource.Id.textView1);
 
-            button.Click
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+            button.Click += delegate
+            {
+                text.Text = "EKRIKURV";
+            };
+            
         }
     }
 }
